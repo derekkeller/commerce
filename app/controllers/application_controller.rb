853d90e6574620ebccd0c4821edfb3888ab419d6
Derek Keller <derekbkeller@gmail.com>
2011-04-20 19:31:07 -0600

    Continued progress on adding rollovers etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :define_layout
+  
+  def define_layout
+    if current_user.admin == true
+      'pages'
+    else
+      'pedros'
+    end
+  end
+
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,6 @@
 class PagesController < ApplicationController
 
-  layout :right_layout
-  
   def index
   end
-  
-  private
-    def right_layout
-      if user_signed_in?
-        'pages'
-      else
-        'pedros'
-      end
-    end
 
 end
